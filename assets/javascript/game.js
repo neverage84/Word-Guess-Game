b4d7513698363ad6088c2a81a827c30ecd4acf00
Noelle Everage <neverage84@yahoo.com>
2019-09-12 14:11:16 -0700

    Added images for background. Updated CSS for images. Added divs for game main content, letters, and buttons. Added arrays for buttons and guesse words.

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+
+<audio id="electrical_parade" src="../javascript/MainStreetElectricalParade.mp3" loop="loop"></audio> 
+
+window.onload = function(){
+    document.getElementById("electrical_parade").play();
+}
+
+alert("User guess: ");
