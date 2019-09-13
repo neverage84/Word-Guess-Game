5097daf0a07cce6b9c55c12e43451146dedc1d6e
Noelle Everage <neverage84@yahoo.com>
2019-09-12 18:01:22 -0700

    Added event listener and array from buttons

--- assets/javascript/game.js
+++
@@ -1,8 +0,0 @@
-
-<audio id="electrical_parade" src="../javascript/MainStreetElectricalParade.mp3" loop="loop"></audio> 
-
-window.onload = function(){
-    document.getElementById("electrical_parade").play();
-}
-
-alert("User guess: ");
